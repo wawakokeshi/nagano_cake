4839873b6babd64d4ec1fab24d02868addaf5058
wawakokeshi <97097377+wawakokeshi@users.noreply.github.com>
2022-02-27 11:56:57 +0000

    [Add]管理者のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,14 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+namespace :admin do
+ root to: 'homes#top'
+ resources :items, except: [:destroy]
+ resources :genres, only: [:index, :create, :edit, :update]
+ resources :customers, only: [:index, :show, :edit, :update]
+ resources :orders, only: [:show, :update]
+ resources :orders_details, only: [:update]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
