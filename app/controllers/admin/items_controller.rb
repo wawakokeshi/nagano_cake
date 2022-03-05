63b1d9f2cf4dc18b86467deb2e4fb32ad2f01655
wawakokeshi <97097377+wawakokeshi@users.noreply.github.com>
2022-03-05 07:41:18 +0000

    [Update]管理者用ページのhtml編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,4 +2,17 @@ class Admin::ItemsController < ApplicationController
  def index
   @items = Item.all
  end
+ 
+ def show
+  @item = Item.find(params[:id])
+ end
+ 
+ def edit
+  @item = Item.find(params[:id])
+ end
+ 
+ def update
+  @item = Item.find(params[:id])
+ end
+ 
 end
