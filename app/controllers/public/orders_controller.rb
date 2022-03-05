453c42e3b91ab22bc7f1fd6b1c84a9453c919321
wawakokeshi <97097377+wawakokeshi@users.noreply.github.com>
2022-03-05 12:29:28 +0000

    [Add]エンドユーザー側のコントローラーとビューの作成

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,22 @@
+class Public::OrdersController < ApplicationController
+
+def new
+end
+
+def show
+end
+
+def index
+end
+
+
+def create
+end
+
+def confirm
+end
+
+def thanks
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,17 @@ namespace :admin do
  resources :orders, only: [:show, :update]
  resources :orders_details, only: [:update]
 end
+
+namespace :public do
+ root to: 'homes#top'
+ resources :items, only: [:index, :show]
+ resources :cart_items, only: [:index, :create, :destroy, :update]
+ resources :customers, only: [:index, :show, :edit, :update]
+ resources :orders, only: [:show, :create, :index]
+ resources :addresses, except: [:show]
+end
+ 
+ 
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
