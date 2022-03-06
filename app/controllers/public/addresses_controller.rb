bae7947c83764a4a2cd8d56dedfb275f39584a6e
wawakokeshi <97097377+wawakokeshi@users.noreply.github.com>
2022-03-06 02:28:30 +0000

    [Update]エンドユーザー側のコントローラー、html内容更新

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,18 +1,24 @@
 class Public::AddressesController < ApplicationController
 
 def index
+ @address = Address.new
+ @addresses = Address.all
 end
 
 def edit
+ @address = Address.find(params[:id])
 end
 
 def create
+ @address = Address.find(params[:id])
 end
 
 def update
+ @address = Address.find(params[:id])
 end
 
 def destroy
+ @address = Address.find(params[:id])
 end
 
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,12 +1,15 @@
 class Public::OrdersController < ApplicationController
 
 def new
+ @order = Order.new
 end
 
 def show
+ @order = Order.find(params[])
 end
 
 def index
+ @orders = Order.all
 end
 
 
